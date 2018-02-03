57b56e4644b21abe817f19ad1f5a3e5269e94d3d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-02-03 05:01:03 -0800

    + comparison-objecgt-views.sql

---
+++ administration/schemas/sys/objects/views/comparison-object-views.sql
@@ -0,0 +1,4 @@
+print "sys.all_objects"   ; select object_definition(object_id('sys.all_objects'   ))
+print "sys.objects"       ; select object_definition(object_id('sys.objects'       ))
+print "sys.sysobjects"    ; select object_definition(object_id('sys.sysobjects'    ))
+print "sys.system_objects"; select object_definition(object_id('sys.system_objects'))
