49cc8ebfe1af6ca9f55ed64c592382a69758a68d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-02-05 11:09:30 -0800

    sqlcmdmaxvartypewidth

--- administration/schemas/sys/objects/views/comparison-object-views.sql
+++ administration/schemas/sys/objects/views/comparison-object-views.sql
@@ -1,3 +1,11 @@
+--
+-- Setting sqlcmdmaxvartypewidth to 0 so as to not limit
+-- the number of characters that are returned by object_definition().
+-- This scripting variable could also be set with sqlcmd's option -y.
+--
+:setvar sqlcmdmaxvartypewidth 0
+
+
 print "sys.all_objects"   ; select object_definition(object_id('sys.all_objects'   ))
 print "sys.objects"       ; select object_definition(object_id('sys.objects'       ))
 print "sys.sysobjects"    ; select object_definition(object_id('sys.sysobjects'    ))
