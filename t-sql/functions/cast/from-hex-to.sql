2f51ecc72dd35565f2ce85edfb470b20deb418ae
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-04-04 17:42:50 +0200

    + from-hex-to.sql

---
+++ t-sql/functions/cast/from-hex-to.sql
@@ -0,0 +1,6 @@
+select
+  cast(0x0104             as integer ) two_hundred_and_sixty,
+  cast(0x843f0b           as date    ) april_8th_2019,
+  cast(0x520065006e00e900 as nvarchar) rene,
+  cast(0x604f7d59         as nvarchar) ni_hao
+;
