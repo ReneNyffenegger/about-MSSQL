d4eff0dc5b23eddcf2f15c26aefa30f762ef89ab
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-04-04 13:50:43 +0200

    Update datename.sql

--- t-sql/functions/datename.sql
+++ t-sql/functions/datename.sql
@@ -1,10 +1,10 @@
 select
-  datename(yyyy, sysdatetime()) year,
-  datename(mm  , sysdatetime()) month,
-  datename(d   , sysdatetime()) day,
-  datename(hh  , sysdatetime()) hour,
-  datename(mi  , sysdatetime()) minute,
-  datename(ss  , sysdatetime()) second,
+  datename(yyyy, sysdatetime()) year       ,
+  datename(mm  , sysdatetime()) month      ,  -- Returns name of month, not its number!
+  datename(d   , sysdatetime()) day        ,
+  datename(hh  , sysdatetime()) hour       ,
+  datename(mi  , sysdatetime()) minute     ,
+  datename(ss  , sysdatetime()) second     ,
   --
   datename(dy  , sysdatetime()) day_of_year	
 ;
