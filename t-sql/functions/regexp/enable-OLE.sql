5ce796a1b80de00fc652c199f3b68e856c28b1d3
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-10 14:11:09 +0200

    + regexp_replace

---
+++ t-sql/functions/regexp/enable-OLE.sql
@@ -0,0 +1,17 @@
+sp_configure 'show advanced options'    , 1; 
+go
+
+reconfigure; 
+go
+
+sp_configure 'Ole Automation Procedures', 1; 
+go
+
+reconfigure; 
+go
+
+sp_configure 'show advanced options'    , 0; 
+go
+
+reconfigure;
+go
---
+++ t-sql/functions/regexp/replace.sql
@@ -0,0 +1,56 @@
+drop   function dbo.regexp_replace;
+
+create function dbo.regexp_replace (
+ --
+ -- https://stackoverflow.com/a/38462603/180275
+ --
+ -- These match exactly the parameters of RegExp
+ --
+    @searchstring  varchar(4000),
+    @pattern       varchar(4000),
+    @replacestring varchar(4000) 
+)
+returns varchar(4000)
+as
+begin
+
+    declare @objRegexExp int, 
+        @objErrorObj     int,
+        @strErrorMessage varchar(255),
+        @res             int,
+        @result          varchar(4000)
+
+    if @searchstring is null or len(ltrim(rtrim(@searchstring))) = 0 begin
+       return null
+    end;
+
+    set @result='';
+
+    exec @res = sp_OACreate 'VBScript.RegExp', @objRegexExp out;
+
+    if @res <> 0 begin
+       return 'VBScript did not initialize!';
+    end;
+
+    exec @res=sp_OASetProperty @objRegexExp, 'Pattern', @pattern;
+
+    if @res <> 0 begin
+       return 'Pattern property set failed!';
+    end;
+
+    exec @res=sp_OASetProperty @objRegexExp, 'IgnoreCase', 0;
+
+    if @res <> 0 begin
+       return 'IgnoreCase option failed!';
+    end;
+
+    exec @res=sp_OAMethod @objRegexExp, 'Replace', @result OUT, @searchstring, @replacestring;
+
+    if @res <> 0 begin
+       return 'Bad search string!';
+    end;
+
+    exec @res=sp_OADestroy @objRegexExp;
+
+    return @result
+end;
