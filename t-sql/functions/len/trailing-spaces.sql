290fab57050efc09cb05b48e1c0b987698df4876
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-08-26 07:31:05 +0200

    Create trailing-spaces.sql

---
+++ t-sql/functions/len/trailing-spaces.sql
@@ -0,0 +1,6 @@
+print(len       ('foo'   )); -- 3
+print(datalength('foo'   )); -- 3
+print(len       ('bar   ')); -- 3 (!)
+print(datalength('bar   ')); -- 6
+print(len       ('   baz')); -- 6
+print(datalength('   baz')); -- 6
