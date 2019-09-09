88ae48f8ced231a2f2f56168292fcc1817e97056
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-09 13:01:20 +0200

    Update example_regexp_replace.sql

--- t-sql/functions/regexp/example_regexp_replace.sql
+++ t-sql/functions/regexp/example_regexp_replace.sql
@@ -3,3 +3,5 @@ select dbo.regexp_replace(
            '([^\s]*)\s*([^\s]*)\s*([^\s]*)',
            'first word: $1, second word: 2: $2, third word: $3, dollar sign: $$.'
        );
+--
+-- first word: foo, second word: 2: bar, third word: baz, dollar sign: $.
