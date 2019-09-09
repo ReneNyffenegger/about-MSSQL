b1615a75f0665155315eee692d9fadc1efb48884
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-09 10:20:12 +0200

    Create select-all.sql

---
+++ administration/schemas/INFORMATION_SCHEMA/schemas/select-all.sql
@@ -0,0 +1,9 @@
+select
+   catalog_name,
+   schema_name,
+   schema_owner,
+   default_character_set_catalog,
+   default_character_set_schema,
+   default_character_set_name
+from
+   information_schema.schemata;
