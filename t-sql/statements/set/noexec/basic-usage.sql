2882046d3cb79faf5426ce3481d836aa569b3c3c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-16 10:39:46 +0200

    Create basic-usage.sql

---
+++ t-sql/statements/set/noexec/basic-usage.sql
@@ -0,0 +1,16 @@
+print ('First batch');
+go
+
+print ('Second batch, first print statement');
+set noexec on
+print ('Second batch, second print statement');
+go
+
+print ('Third batch, first print statement');
+set noexec off
+print ('Third batch, second print statement');
+go
+
+-- First batch
+-- Second batch, first print statement
+-- Third batch, second print statement
