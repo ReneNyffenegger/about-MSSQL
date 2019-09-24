e088bf8222d67bce0338afbbf248daccdc2a2069
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-24 15:42:41 +0200

    Update add-jobserver.sql

--- architecture/services/agent/jobs/examples/simple/add-jobserver.sql
+++ architecture/services/agent/jobs/examples/simple/add-jobserver.sql
@@ -1,6 +1,3 @@
 exec msdb.dbo.sp_add_jobserver
    @job_name    = N'tq84_job_example_job',
    @server_name = @@servername
-
---  SQLServerAgent is not currently running so it cannot be notified of this action.
---
