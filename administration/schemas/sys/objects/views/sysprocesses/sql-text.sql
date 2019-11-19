21b10604d6fc2191955428520bb5f0146459e5a2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-19 10:25:10 +0100

    Create sql-text.sql

---
+++ administration/schemas/sys/objects/views/sysprocesses/sql-text.sql
@@ -0,0 +1,13 @@
+select
+   prc.login_time               ,
+   prc.last_batch               ,
+   db_name(prc.dbid)    db_name ,
+   suser_sname(prc.sid) sid_    , 
+   prc.nt_username              ,
+   prc.loginame                 ,   -- Note: it's loginame, not loginname.
+   prc.hostname                 ,
+   prc.program_name             ,
+   sql.text             sql_text
+from
+   sys.sysprocesses     prc  cross apply
+   sys.dm_exec_sql_text(prc.sql_handle) sql;
