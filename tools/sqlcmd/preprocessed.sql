920fe0b95aae03510cf3e5d172d0e45e2379a35d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-07 14:53:41 +0100

    + preprocessed

---
+++ tools/sqlcmd/preprocessed.sql
@@ -0,0 +1,13 @@
+if 3<5 begin
+
+   print("This text is printed");
+  :setVar txt "first assignment"
+
+end else begin
+
+   print("This text is not printed");
+  :setVar txt "second assignment"
+
+end
+
+print("$(txt)");
