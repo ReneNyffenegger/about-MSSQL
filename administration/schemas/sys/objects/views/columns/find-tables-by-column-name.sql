736581ff193bff3e50160f811bc85f2825c73252
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-10-19 11:47:18 +0200

    select schema

--- administration/schemas/sys/objects/views/columns/find-tables-by-column-name.sql
+++ administration/schemas/sys/objects/views/columns/find-tables-by-column-name.sql
@@ -1,8 +1,10 @@
 select
-  tab.name   tab_name,
-  col.name   col_name
+   tab.name    table_,
+   sch.name    schema_,
+   col.name    column_
 from
-  sys.tables  tab join
-  sys.columns col on tab.object_id = col.object_id
+   sys.tables  tab                                    join
+   sys.columns col on tab.object_id = col.object_id   join
+   sys.schemas sch on tab.schema_id = sch.schema_id
 where
-  col.name like '%FOO%';
+   col.name like '%ABC%';
