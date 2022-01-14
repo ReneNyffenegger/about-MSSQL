02b47020f53642580fa20b9a7309b96f785d1fbe
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-14 09:39:48 +0100

    mv to sql server helpers

--- t-sql/functions/regexp/example_regexp_replace.sql
+++
@@ -1,7 +0,0 @@
-select dbo.regexp_replace(
-           'foo bar baz',
-           '([^\s]*)\s*([^\s]*)\s*([^\s]*)',
-           'first word: $1, second word: 2: $2, third word: $3, dollar sign: $$.'
-       );
---
--- first word: foo, second word: 2: bar, third word: baz, dollar sign: $.
