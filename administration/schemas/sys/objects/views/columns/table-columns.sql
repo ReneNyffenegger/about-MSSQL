f6661a57cb5ea586e1059b0f606ec5c9fcae115c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-03-08 13:21:42 +0100

    Update table-columns.sql

--- administration/schemas/sys/objects/views/columns/table-columns.sql
+++ administration/schemas/sys/objects/views/columns/table-columns.sql
@@ -3,15 +3,15 @@ select
    tab.name             tab_name,
    col.name             col_name,
 -- col.user_type_id,
-   ust.name             user_type,
+   ust.name             user_type
 -- col.system_type_id,
-   syt.name             system_type
+-- syt.name             system_type
 from
-   sys.schemas   sch                                          join
-   sys.tables    tab on sch.schema_id      = tab.schema_id    join
-   sys.columns   col on tab.object_id      = col.object_id    join
-   sys.types     ust on col.user_type_id   = ust.user_type_id join
-   sys.types     syt on col.system_type_id = syt.system_type_id
+   sys.schemas   sch                                               join
+   sys.tables    tab on sch.schema_id      = tab.schema_id         join
+   sys.columns   col on tab.object_id      = col.object_id         join
+   sys.types     ust on col.user_type_id   = ust.user_type_id   -- join
+-- sys.types     syt on col.system_type_id = syt.system_type_id
 where
    sch.name = 'dbo'                  and
    tab.name = 'foo_bar'
